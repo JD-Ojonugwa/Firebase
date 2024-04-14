ca6baa3fee9c851026c655c019dc8fc4e7fc8751
JD-Ojonugwa <joydorothy@gmail.com>
2024-04-14 20:35:11 +0100

    feat: installed tailwind, daisyui and firebase. Also created a component that has layout and pages.

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./component/layout/Card"
+
+
+function App() {
+ 
+
+  return (
+    <>
+       <div className="flex justify-center mt-10">
+        <Card />
+      </div>
+    </>
+  );
+}
+
+export default App
